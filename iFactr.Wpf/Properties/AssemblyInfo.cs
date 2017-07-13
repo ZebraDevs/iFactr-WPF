093dbb375ce36b1cf44372231abcdef47c1ea9b6
Steve Rawlins <srawlins@zebra.com>
2017-07-13 15:43:41 -0500

    Update Copyright year to 2017

--- iFactr.Wpf/Properties/AssemblyInfo.cs
+++ iFactr.Wpf/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Zebra Technologies Corporation")]
 [assembly: AssemblyProduct("iFactr.Wpf")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
